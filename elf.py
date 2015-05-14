6c7e4884d7eeb1d372d3e3f648e7ea4ea464f6e7
Pasha Kravtsov <pk-man@users.noreply.github.com>
2015-05-14 11:52:55 -0700

    Update elf.py
    List comprehension.

--- elf.py
+++ elf.py
@@ -14,8 +14,6 @@ if __name__ == '__main__':
 		print 'Size of shellcode: [ %s ]' % section['sh_size']
 		raw = section.data().encode('hex')
 		shellcode = ''
-		bytes = [ raw[i:i+2] for i in range(0, len(raw), 2) ]
-		for x in bytes:
-			shellcode += r'\x' + x
+		bytes = [ r'\x' + raw[i:i+2] for i in range(0, len(raw), 2) ]
 		
 		print 'Shellcode: %s' % shellcode
