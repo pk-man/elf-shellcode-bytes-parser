4e046d581bda1a556d8e5d96071faeabd06c32fc
Pasha Kravtsov <pk-man@users.noreply.github.com>
2015-05-14 11:57:18 -0700

    Update elf.py
    Made it smaller.

--- elf.py
+++ elf.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python
-import sys
+import sys 
 
 from elftools.elf.elffile import ELFFile
 
 if __name__ == '__main__':
-	
-	if len(sys.argv) > 2 or len(sys.argv) == 1:
-		print 'Usage: %s <file>' % sys.argv[0]
-	
-	with open(sys.argv[1], 'rb') as stream:
-		elffile = ELFFile(stream)
-		section = elffile.get_section_by_name(b'.text')
-		print 'Size of shellcode: [ %s ]' % section['sh_size']
-		raw = section.data().encode('hex')
-		shellcode = ''
-		bytes = [ r'\x' + raw[i:i+2] for i in range(0, len(raw), 2) ]
-		
-		print 'Shellcode: %s' % shellcode
+    
+    if len(sys.argv) > 2 or len(sys.argv) == 1:
+        print 'Usage: %s <file>' % sys.argv[0]
+    
+    with open(sys.argv[1], 'rb') as stream:
+        elffile = ELFFile(stream)
+        section = elffile.get_section_by_name(b'.text')
+        print 'Size of shellcode: [ %s ]' % section['sh_size']
+        raw = section.data().encode('hex')
+        bytes = [ r'\x' + raw[i:i+2] for i in range(0, len(raw), 2) ]
+        print 'Shellcode: %s' % ''.join(bytes)
